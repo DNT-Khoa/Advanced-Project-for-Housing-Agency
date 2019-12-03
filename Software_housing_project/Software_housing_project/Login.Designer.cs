640a69567803d2685e14d6ec49a0f85fc2e4f779
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2019-12-03 21:48:23 +0100

    Login Form calls MainForm

---
+++ Software_housing_project/Software_housing_project/Login.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace Software_housing_project
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cbxUserType = new System.Windows.Forms.ComboBox();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.pbxUserIcon = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUserIcon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cbxUserType
+            // 
+            this.cbxUserType.FormattingEnabled = true;
+            this.cbxUserType.Items.AddRange(new object[] {
+            "Student",
+            "Employee"});
+            this.cbxUserType.Location = new System.Drawing.Point(117, 180);
+            this.cbxUserType.Name = "cbxUserType";
+            this.cbxUserType.Size = new System.Drawing.Size(118, 24);
+            this.cbxUserType.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Location = new System.Drawing.Point(148, 214);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(60, 33);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.BtnLogin_Click);
+            // 
+            // pbxUserIcon
+            // 
+            this.pbxUserIcon.Location = new System.Drawing.Point(104, 33);
+            this.pbxUserIcon.Name = "pbxUserIcon";
+            this.pbxUserIcon.Size = new System.Drawing.Size(147, 141);
+            this.pbxUserIcon.TabIndex = 3;
+            this.pbxUserIcon.TabStop = false;
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 267);
+            this.Controls.Add(this.pbxUserIcon);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.cbxUserType);
+            this.Name = "Login";
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pbxUserIcon)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cbxUserType;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.PictureBox pbxUserIcon;
+    }
+}
---
+++ Software_housing_project/Software_housing_project/Login.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Software_housing_project
+{
+    public partial class Login : Form
+    {
+        public static bool isEmployee = false;
+        MainForm mf;
+
+        public Login()
+        {
+            InitializeComponent();
+            cbxUserType.SelectedIndex = 0;
+        }
+
+        private void BtnLogin_Click(object sender, EventArgs e)
+        {
+            string text = cbxUserType.Text;
+            switch (text)
+            {
+                case "Employee": isEmployee = true; break;
+                default: isEmployee = false; break;
+            }
+            mf = new MainForm();
+            mf.Visible = true;
+        }
+    }
+}
