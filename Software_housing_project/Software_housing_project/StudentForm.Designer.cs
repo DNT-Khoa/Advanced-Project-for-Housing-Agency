4dcc656c5f8ac444e88195cfcff655351b618ea7
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2019-12-04 15:01:13 +0100

    testing tab control

--- Software_housing_project/Software_housing_project/StudentForm.Designer.cs
+++ Software_housing_project/Software_housing_project/StudentForm.Designer.cs
@@ -31,6 +31,7 @@
             this.tabControl1 = new System.Windows.Forms.TabControl();
             this.tpChores = new System.Windows.Forms.TabPage();
             this.tpComplaints = new System.Windows.Forms.TabPage();
+            this.tpRules = new System.Windows.Forms.TabPage();
             this.tabControl1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -38,39 +39,53 @@
             // 
             this.tabControl1.Controls.Add(this.tpChores);
             this.tabControl1.Controls.Add(this.tpComplaints);
-            this.tabControl1.Location = new System.Drawing.Point(3, 12);
+            this.tabControl1.Controls.Add(this.tpRules);
+            this.tabControl1.Location = new System.Drawing.Point(4, 15);
+            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tabControl1.Name = "tabControl1";
             this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(596, 353);
+            this.tabControl1.Size = new System.Drawing.Size(795, 434);
             this.tabControl1.TabIndex = 0;
             // 
             // tpChores
             // 
-            this.tpChores.Location = new System.Drawing.Point(4, 22);
+            this.tpChores.Location = new System.Drawing.Point(4, 25);
+            this.tpChores.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tpChores.Name = "tpChores";
-            this.tpChores.Padding = new System.Windows.Forms.Padding(3);
-            this.tpChores.Size = new System.Drawing.Size(588, 327);
+            this.tpChores.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tpChores.Size = new System.Drawing.Size(787, 405);
             this.tpChores.TabIndex = 0;
             this.tpChores.Text = "Chores";
             this.tpChores.UseVisualStyleBackColor = true;
             // 
             // tpComplaints
             // 
-            this.tpComplaints.Location = new System.Drawing.Point(4, 22);
+            this.tpComplaints.Location = new System.Drawing.Point(4, 25);
+            this.tpComplaints.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.tpComplaints.Name = "tpComplaints";
-            this.tpComplaints.Padding = new System.Windows.Forms.Padding(3);
-            this.tpComplaints.Size = new System.Drawing.Size(588, 327);
+            this.tpComplaints.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tpComplaints.Size = new System.Drawing.Size(787, 405);
             this.tpComplaints.TabIndex = 1;
             this.tpComplaints.Text = "Complaints";
             this.tpComplaints.UseVisualStyleBackColor = true;
             // 
+            // tpRules
+            // 
+            this.tpRules.Location = new System.Drawing.Point(4, 25);
+            this.tpRules.Name = "tpRules";
+            this.tpRules.Padding = new System.Windows.Forms.Padding(3);
+            this.tpRules.Size = new System.Drawing.Size(787, 405);
+            this.tpRules.TabIndex = 2;
+            this.tpRules.Text = "Rules";
+            this.tpRules.UseVisualStyleBackColor = true;
+            // 
             // StudentForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
+            this.ClientSize = new System.Drawing.Size(800, 450);
             this.Controls.Add(this.tabControl1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "StudentForm";
             this.Text = "HouseMate";
             this.tabControl1.ResumeLayout(false);
@@ -83,6 +98,7 @@
         private System.Windows.Forms.TabControl tabControl1;
         private System.Windows.Forms.TabPage tpChores;
         private System.Windows.Forms.TabPage tpComplaints;
+        private System.Windows.Forms.TabPage tpRules;
     }
 }
 
