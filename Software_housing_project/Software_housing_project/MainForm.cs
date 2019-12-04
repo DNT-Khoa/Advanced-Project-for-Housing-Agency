3f79a3abba7114efffc1ec6c836f66cf0f8a9527
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2019-12-04 14:34:33 +0100

    removed getinfo sample code

--- Software_housing_project/Software_housing_project/MainForm.cs
+++ Software_housing_project/Software_housing_project/MainForm.cs
@@ -15,8 +15,6 @@ namespace Software_housing_project
         public MainForm()
         {
             InitializeComponent();
-            Student student = new Student("Daniel", "abc", 19, "Fontys", "ICT");
-            MessageBox.Show(student.GetInfo());
         }
     }
 }
