e78f1c2b16ce5e226b95fd06f77d7f0e6833fc32
Vaswani,Daniel D <d.vaswani@student.fontys.nl>
2019-12-05 12:15:37 +0100

    Made Student GUI

--- Software_housing_project/Software_housing_project/StudentForm.Designer.cs
+++ Software_housing_project/Software_housing_project/StudentForm.Designer.cs
@@ -28,77 +28,218 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tcTabControl = new System.Windows.Forms.TabControl();
             this.tpChores = new System.Windows.Forms.TabPage();
             this.tpComplaints = new System.Windows.Forms.TabPage();
             this.tpRules = new System.Windows.Forms.TabPage();
-            this.tabControl1.SuspendLayout();
+            this.cbxName = new System.Windows.Forms.ComboBox();
+            this.cbxChore = new System.Windows.Forms.ComboBox();
+            this.mcChore = new System.Windows.Forms.MonthCalendar();
+            this.btnAddChore = new System.Windows.Forms.Button();
+            this.clbChores = new System.Windows.Forms.CheckedListBox();
+            this.rtbDescription = new System.Windows.Forms.RichTextBox();
+            this.btnFileComplaint = new System.Windows.Forms.Button();
+            this.mcComplaint = new System.Windows.Forms.MonthCalendar();
+            this.rtbComplaints = new System.Windows.Forms.RichTextBox();
+            this.btnPrevious = new System.Windows.Forms.Button();
+            this.btnNext = new System.Windows.Forms.Button();
+            this.lbxRules = new System.Windows.Forms.ListBox();
+            this.tcTabControl.SuspendLayout();
+            this.tpChores.SuspendLayout();
+            this.tpComplaints.SuspendLayout();
+            this.tpRules.SuspendLayout();
             this.SuspendLayout();
             // 
-            // tabControl1
+            // tcTabControl
             // 
-            this.tabControl1.Controls.Add(this.tpChores);
-            this.tabControl1.Controls.Add(this.tpComplaints);
-            this.tabControl1.Controls.Add(this.tpRules);
-            this.tabControl1.Location = new System.Drawing.Point(4, 15);
-            this.tabControl1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tabControl1.Name = "tabControl1";
-            this.tabControl1.SelectedIndex = 0;
-            this.tabControl1.Size = new System.Drawing.Size(795, 434);
-            this.tabControl1.TabIndex = 0;
+            this.tcTabControl.Controls.Add(this.tpChores);
+            this.tcTabControl.Controls.Add(this.tpComplaints);
+            this.tcTabControl.Controls.Add(this.tpRules);
+            this.tcTabControl.Location = new System.Drawing.Point(4, 13);
+            this.tcTabControl.Margin = new System.Windows.Forms.Padding(4);
+            this.tcTabControl.Name = "tcTabControl";
+            this.tcTabControl.SelectedIndex = 0;
+            this.tcTabControl.Size = new System.Drawing.Size(844, 434);
+            this.tcTabControl.TabIndex = 0;
             // 
             // tpChores
             // 
+            this.tpChores.Controls.Add(this.clbChores);
+            this.tpChores.Controls.Add(this.btnAddChore);
+            this.tpChores.Controls.Add(this.mcChore);
+            this.tpChores.Controls.Add(this.cbxChore);
+            this.tpChores.Controls.Add(this.cbxName);
             this.tpChores.Location = new System.Drawing.Point(4, 25);
-            this.tpChores.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tpChores.Margin = new System.Windows.Forms.Padding(4);
             this.tpChores.Name = "tpChores";
-            this.tpChores.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tpChores.Size = new System.Drawing.Size(787, 405);
+            this.tpChores.Padding = new System.Windows.Forms.Padding(4);
+            this.tpChores.Size = new System.Drawing.Size(836, 405);
             this.tpChores.TabIndex = 0;
             this.tpChores.Text = "Chores";
             this.tpChores.UseVisualStyleBackColor = true;
             // 
             // tpComplaints
             // 
+            this.tpComplaints.Controls.Add(this.btnNext);
+            this.tpComplaints.Controls.Add(this.btnPrevious);
+            this.tpComplaints.Controls.Add(this.rtbComplaints);
+            this.tpComplaints.Controls.Add(this.mcComplaint);
+            this.tpComplaints.Controls.Add(this.btnFileComplaint);
+            this.tpComplaints.Controls.Add(this.rtbDescription);
             this.tpComplaints.Location = new System.Drawing.Point(4, 25);
-            this.tpComplaints.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.tpComplaints.Margin = new System.Windows.Forms.Padding(4);
             this.tpComplaints.Name = "tpComplaints";
-            this.tpComplaints.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.tpComplaints.Size = new System.Drawing.Size(787, 405);
+            this.tpComplaints.Padding = new System.Windows.Forms.Padding(4);
+            this.tpComplaints.Size = new System.Drawing.Size(836, 405);
             this.tpComplaints.TabIndex = 1;
             this.tpComplaints.Text = "Complaints";
             this.tpComplaints.UseVisualStyleBackColor = true;
             // 
             // tpRules
             // 
+            this.tpRules.Controls.Add(this.lbxRules);
             this.tpRules.Location = new System.Drawing.Point(4, 25);
             this.tpRules.Name = "tpRules";
             this.tpRules.Padding = new System.Windows.Forms.Padding(3);
-            this.tpRules.Size = new System.Drawing.Size(787, 405);
+            this.tpRules.Size = new System.Drawing.Size(836, 405);
             this.tpRules.TabIndex = 2;
             this.tpRules.Text = "Rules";
             this.tpRules.UseVisualStyleBackColor = true;
+            // 
+            // cbxName
+            // 
+            this.cbxName.FormattingEnabled = true;
+            this.cbxName.Location = new System.Drawing.Point(23, 38);
+            this.cbxName.Name = "cbxName";
+            this.cbxName.Size = new System.Drawing.Size(142, 24);
+            this.cbxName.TabIndex = 0;
+            this.cbxName.Text = "Your Name";
+            // 
+            // cbxChore
+            // 
+            this.cbxChore.FormattingEnabled = true;
+            this.cbxChore.Location = new System.Drawing.Point(23, 78);
+            this.cbxChore.Name = "cbxChore";
+            this.cbxChore.Size = new System.Drawing.Size(142, 24);
+            this.cbxChore.TabIndex = 1;
+            this.cbxChore.Text = "Chore";
+            // 
+            // mcChore
+            // 
+            this.mcChore.Location = new System.Drawing.Point(220, 38);
+            this.mcChore.Name = "mcChore";
+            this.mcChore.TabIndex = 2;
+            // 
+            // btnAddChore
+            // 
+            this.btnAddChore.Location = new System.Drawing.Point(23, 339);
+            this.btnAddChore.Name = "btnAddChore";
+            this.btnAddChore.Size = new System.Drawing.Size(142, 40);
+            this.btnAddChore.TabIndex = 3;
+            this.btnAddChore.Text = "Add Chore";
+            this.btnAddChore.UseVisualStyleBackColor = true;
+            // 
+            // clbChores
+            // 
+            this.clbChores.FormattingEnabled = true;
+            this.clbChores.Location = new System.Drawing.Point(520, 38);
+            this.clbChores.Name = "clbChores";
+            this.clbChores.Size = new System.Drawing.Size(263, 310);
+            this.clbChores.TabIndex = 4;
+            // 
+            // rtbDescription
+            // 
+            this.rtbDescription.Location = new System.Drawing.Point(17, 25);
+            this.rtbDescription.Name = "rtbDescription";
+            this.rtbDescription.Size = new System.Drawing.Size(244, 299);
+            this.rtbDescription.TabIndex = 0;
+            this.rtbDescription.Text = "Description";
+            // 
+            // btnFileComplaint
+            // 
+            this.btnFileComplaint.Location = new System.Drawing.Point(17, 343);
+            this.btnFileComplaint.Name = "btnFileComplaint";
+            this.btnFileComplaint.Size = new System.Drawing.Size(162, 41);
+            this.btnFileComplaint.TabIndex = 1;
+            this.btnFileComplaint.Text = "File Complaint";
+            this.btnFileComplaint.UseVisualStyleBackColor = true;
+            // 
+            // mcComplaint
+            // 
+            this.mcComplaint.Location = new System.Drawing.Point(273, 25);
+            this.mcComplaint.Name = "mcComplaint";
+            this.mcComplaint.TabIndex = 2;
+            // 
+            // rtbComplaints
+            // 
+            this.rtbComplaints.Location = new System.Drawing.Point(547, 25);
+            this.rtbComplaints.Name = "rtbComplaints";
+            this.rtbComplaints.Size = new System.Drawing.Size(263, 299);
+            this.rtbComplaints.TabIndex = 3;
+            this.rtbComplaints.Text = "Complaint";
+            // 
+            // btnPrevious
+            // 
+            this.btnPrevious.Location = new System.Drawing.Point(547, 343);
+            this.btnPrevious.Name = "btnPrevious";
+            this.btnPrevious.Size = new System.Drawing.Size(75, 23);
+            this.btnPrevious.TabIndex = 4;
+            this.btnPrevious.Text = "Previous";
+            this.btnPrevious.UseVisualStyleBackColor = true;
+            // 
+            // btnNext
+            // 
+            this.btnNext.Location = new System.Drawing.Point(735, 343);
+            this.btnNext.Name = "btnNext";
+            this.btnNext.Size = new System.Drawing.Size(75, 23);
+            this.btnNext.TabIndex = 5;
+            this.btnNext.Text = "Next";
+            this.btnNext.UseVisualStyleBackColor = true;
+            // 
+            // lbxRules
+            // 
+            this.lbxRules.FormattingEnabled = true;
+            this.lbxRules.ItemHeight = 16;
+            this.lbxRules.Location = new System.Drawing.Point(228, 38);
+            this.lbxRules.Name = "lbxRules";
+            this.lbxRules.Size = new System.Drawing.Size(375, 340);
+            this.lbxRules.TabIndex = 0;
             // 
             // StudentForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.tabControl1);
+            this.ClientSize = new System.Drawing.Size(846, 450);
+            this.Controls.Add(this.tcTabControl);
             this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "StudentForm";
             this.Text = "HouseMate";
-            this.tabControl1.ResumeLayout(false);
+            this.tcTabControl.ResumeLayout(false);
+            this.tpChores.ResumeLayout(false);
+            this.tpComplaints.ResumeLayout(false);
+            this.tpRules.ResumeLayout(false);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabControl tcTabControl;
         private System.Windows.Forms.TabPage tpChores;
         private System.Windows.Forms.TabPage tpComplaints;
         private System.Windows.Forms.TabPage tpRules;
+        private System.Windows.Forms.CheckedListBox clbChores;
+        private System.Windows.Forms.Button btnAddChore;
+        private System.Windows.Forms.MonthCalendar mcChore;
+        private System.Windows.Forms.ComboBox cbxChore;
+        private System.Windows.Forms.ComboBox cbxName;
+        private System.Windows.Forms.Button btnNext;
+        private System.Windows.Forms.Button btnPrevious;
+        private System.Windows.Forms.RichTextBox rtbComplaints;
+        private System.Windows.Forms.MonthCalendar mcComplaint;
+        private System.Windows.Forms.Button btnFileComplaint;
+        private System.Windows.Forms.RichTextBox rtbDescription;
+        private System.Windows.Forms.ListBox lbxRules;
     }
 }
 
