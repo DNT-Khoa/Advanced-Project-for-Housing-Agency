6071c1c92051cae7aea8c721f0573e869fc21a1b
dntkhoa <dntkhoa@gmail.com>
2020-01-07 15:09:34 +0100

    fixing idCounter

--- Software_housing_project/Software_housing_project/Student.cs
+++ Software_housing_project/Software_housing_project/Student.cs
@@ -8,21 +8,21 @@ namespace Software_housing_project
 {    
     public class Student
     {
-        static int idCounter = 0;
+        private static int idCounter = 0;
         private int Age;
         private string School;
         private string Course;
 
         public string FirstName { get; }
         public string LastName { get; }
-        public int IdNumber { get; }
+        public int IdNumber { get { return idCounter; }}
 
         public Student(string firstName, string lastName, int age, string school,
             string course)
         {
             this.FirstName = firstName;
             this.LastName = lastName;
-            this.IdNumber = idCounter++;
+            idCounter++;
             this.Age = age;
             this.School = school;
             this.Course = course;
